f9854077688a6abb198e44a32400dbe4552181d6
libo-liti <132136249+libo-liti@users.noreply.github.com>
2025-06-04 16:09:58 +0900

    Add font, Add UI

--- Assets/02. Scripts/Cat/CatController.cs
+++ Assets/02. Scripts/Cat/CatController.cs
@@ -1,23 +1,32 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using Cat;
 
 public class CatController : MonoBehaviour
 {
+    public SoundManager soundManager;
+    
     private Rigidbody2D catRb;
+    private Animator CatAnima;
+    
     public float jumpPower = 10f;
     public bool isGround = false;
     public int jumpCount = 0;
     void Start()
     {
         catRb = GetComponent<Rigidbody2D>();
+        CatAnima = GetComponent<Animator>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 2)
         {
+            CatAnima.SetTrigger("Jump");
+            CatAnima.SetBool("isGround", false);
             catRb.AddForceY(jumpPower, ForceMode2D.Impulse);
             jumpCount++;
+            
+            soundManager.OnJumpSound();
         }
     }
 
@@ -25,6 +34,7 @@ public class CatController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Ground"))
         {
+            CatAnima.SetBool("isGround", true);
             isGround = true;
             jumpCount = 0;
         }
